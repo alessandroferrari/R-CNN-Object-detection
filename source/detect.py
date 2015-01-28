65b5f5336e9e839fe88a43571541beb907921ea5
alessandroferrari <alessandroferrari87@gmail.com>
2015-01-28 23:26:54 +0100

    First commit

---
+++ source/detect.py
@@ -0,0 +1,299 @@
+#!/usr/bin/env python
+"""
+detector.py is an out-of-the-box windowed detector
+callable from the command line.
+
+By default it configures and runs the Caffe reference ImageNet model.
+Note that this model was trained for image classification and not detection,
+and finetuning for detection can be expected to improve results.
+
+The selective_search_ijcv_with_python code required for the selective search
+proposal mode is available at
+    https://github.com/sergeyk/selective_search_ijcv_with_python
+
+TODO:
+- batch up image filenames as well: don't want to load all of them into memory
+- come up with a batching scheme that preserved order / keeps a unique ID
+"""
+import os
+import cv2
+import time
+import json
+import caffe
+import argparse
+import numpy as np
+import pandas as pd
+from random import randint
+from skimage.io import imread
+from detector import Detector, resize_image
+
+CROP_MODES = ['bing']
+COORD_COLS = ['ymin', 'xmin', 'ymax', 'xmax']
+
+def nms_detections(dets, overlap=0.3):
+    """
+    Non-maximum suppression: Greedily select high-scoring detections and
+    skip detections that are significantly covered by a previously
+    selected detection.
+
+    This version is translated from Matlab code by Tomasz Malisiewicz,
+    who sped up Pedro Felzenszwalb's code.
+
+    Parameters
+    ----------
+    dets: ndarray
+        each row is ['xmin', 'ymin', 'xmax', 'ymax', 'score']
+    overlap: float
+        minimum overlap ratio (0.3 default)
+
+    Output
+    ------
+    dets: ndarray
+        remaining after suppression.
+    """
+    x1 = dets[:, 0]
+    y1 = dets[:, 1]
+    x2 = dets[:, 2]
+    y2 = dets[:, 3]
+    ind = np.argsort(dets[:, 4])
+
+    w = x2 - x1
+    h = y2 - y1
+    area = (w * h).astype(float)
+
+    pick = []
+    while len(ind) > 0:
+        i = ind[-1]
+        pick.append(i)
+        ind = ind[:-1]
+
+        xx1 = np.maximum(x1[i], x1[ind])
+        yy1 = np.maximum(y1[i], y1[ind])
+        xx2 = np.minimum(x2[i], x2[ind])
+        yy2 = np.minimum(y2[i], y2[ind])
+
+        w = np.maximum(0., xx2 - xx1)
+        h = np.maximum(0., yy2 - yy1)
+
+        wh = w * h
+        o = wh / (area[i] + area[ind] - wh)
+
+        ind = ind[np.nonzero(o <= overlap)[0]]
+
+    return dets[pick, :]
+
+def main(argv):
+    pycaffe_dir = os.path.dirname(__file__)
+
+    parser = argparse.ArgumentParser()
+    # Required arguments: input and output.
+    parser.add_argument(
+        "image_file",
+        help="Input image filename."
+    )
+    parser.add_argument(
+        "output_image_file",
+        help="Output image filename. Format depends on extension."
+    )
+    # Optional arguments.
+    parser.add_argument(
+        "--model_def",
+        default=os.path.join(pycaffe_dir,
+                "../models/bvlc_reference_caffenet/deploy.prototxt.prototxt"),
+        help="Model definition file."
+    )
+    parser.add_argument(
+        "--pretrained_model",
+        default=os.path.join(pycaffe_dir,
+                "../models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel"),
+        help="Trained model weights file."
+    )
+    parser.add_argument(
+        "--crop_mode",
+        default="bing",
+        choices=CROP_MODES,
+        help="How to generate windows for detection."
+    )
+    parser.add_argument(
+        "--weights_1st_stage_bing_fn",
+        default=None,
+        help="Weights generated for the first stage of bing."
+    )
+    parser.add_argument(
+        "--sizes_idx_bing_fn",
+        default=None,
+        help="Indeces of the active sizes for bing."
+    )
+    parser.add_argument(
+        "--weights_2nd_stage_bing_fn",
+        default=None,
+        help="Weights generated for the second stage of bing."
+    )
+    parser.add_argument(
+        "--num_bbs_psz_bing",
+        default=130,
+        type=int,
+        help="Number of bounding boxes per size index in bing."
+    )
+    parser.add_argument(
+        "--num_bbs_final_bing",
+        type=int,
+        default=1500,
+        help="Final number of bounding boxes candidates in bing."
+    )
+    parser.add_argument(
+        "--gpu",
+        action='store_true',
+        help="Switch for gpu computation."
+    )
+    parser.add_argument(
+        "--mean_file",
+        default=os.path.join(pycaffe_dir,
+                             'caffe/imagenet/ilsvrc_2012_mean.npy'),
+        help="Data set image mean of H x W x K dimensions (numpy array). " +
+             "Set to '' for no mean subtraction."
+    )
+    parser.add_argument(
+        "--input_scale",
+        type=float,
+        help="Multiply input features by this scale to finish preprocessing."
+    )
+    parser.add_argument(
+        "--raw_scale",
+        type=float,
+        default=255.0,
+        help="Multiply raw input by this scale before preprocessing."
+    )
+    parser.add_argument(
+        "--channel_swap",
+        default='2,1,0',
+        help="Order to permute input channels. The default converts " +
+             "RGB -> BGR since BGR is the Caffe default by way of OpenCV."
+    )
+    parser.add_argument(
+        "--context_pad",
+        type=int,
+        default='16',
+        help="Amount of surrounding context to collect in input window."
+    )
+    parser.add_argument(
+        "--detection_threshold",
+        type=float,
+        default=0.1,
+        help="Score threshold for determining positive detection from the convolutional network."
+    )
+    parser.add_argument(
+        "synset_file",
+        help="Filename that contains the synset that corresponds to the classes of the network."
+    )
+    
+    args = parser.parse_args()
+
+    synset_dict = dict()
+    f = open(args.synset_file)
+    counter = 0
+    while True:
+        s = f.readline()
+        if s=='':
+            break
+        name = s[s.find(" ")+1:]
+        synset_dict[counter] = name
+        counter = counter + 1
+    f.close()
+
+    if not args.weights_1st_stage_bing_fn is None:
+        if not os.path.exists(args.weights_1st_stage_bing_fn):
+            print "Error the path specified for weights_1st_stage_bing_fn %s does not exist."%args.weights_1st_stage_bing_fn
+            sys.exit(2)
+        weights_1st_stage_bing = np.genfromtxt(args.weights_1st_stage_bing_fn, delimiter=',', dtype=np.float32)
+    else:
+        weights_1st_stage_bing = None
+    
+    if not args.weights_2nd_stage_bing_fn is None:
+        if not os.path.exists(args.weights_2nd_stage_bing_fn):
+            print "Error: the path specified for weights_2nd_stage_bing_fn %s does not exist."%args.weights_2nd_stage_bing_fn
+            sys.exit(2)
+        f = open(args.weights_2nd_stage_bing_fn,"r")
+        weights_2nd_stage_bing_str = f.read()
+        f.close()
+        weights_2nd_stage_bing = json.loads(weights_2nd_stage_bing_str)
+    else:
+        weights_2nd_stage_bing = None
+
+    if not args.sizes_idx_bing_fn is None:
+        if not os.path.exists(args.sizes_idx_bing_fn):
+            print "Error: the path specified for sizes_idx_bing_fn %s does not exists."%args.sizes_idx_bing_fn
+            sys.exit(2)
+        sizes_idx_bing = np.genfromtxt(args.sizes_idx_bing_fn, delimiter=',').astype(np.int32)
+    else:
+        sizes_idx_bing = None
+        
+    mean, channel_swap = None, None
+    if args.mean_file:
+        mean = np.load(args.mean_file)
+    if args.channel_swap:
+        channel_swap = [int(s) for s in args.channel_swap.split(',')]
+
+    # Make detector.
+    detector = Detector(args.model_def, args.pretrained_model,
+            gpu=args.gpu, mean=mean,
+            input_scale=args.input_scale, raw_scale=args.raw_scale,
+            channel_swap=channel_swap,
+            context_pad=args.context_pad,
+            weights_1st_stage_bing = weights_1st_stage_bing, 
+            sizes_idx_bing = sizes_idx_bing,
+            weights_2nd_stage_bing = weights_2nd_stage_bing, 
+            num_bbs_psz_bing = args.num_bbs_psz_bing, 
+            num_bbs_final_bing = args.num_bbs_final_bing)
+
+    if args.gpu:
+        print 'GPU mode'
+
+    t = time.time()
+
+    image_fn = args.image_file
+    image = imread(image_fn)
+    image = resize_image(image)
+
+    if args.crop_mode == "bing":
+        detections, predictions = detector.detect_bing(image)
+    else:
+        print "Crop mode is not supported!"
+        sys.exit(1)
+        
+    print("Processed {} windows in {:.3f} s.".format(len(detections),
+                                                     time.time() - t))
+    
+    predictions[predictions<args.detection_threshold] = 0
+    
+    indices = np.nonzero(predictions)
+    
+    classes = np.unique(indices[1])
+
+    resulting_bbs = []
+    for cls in classes:
+        mask = indices[1]==cls
+        bbs_idx = indices[0][mask]
+        bbs_for_nms = [ np.array( (detections[idx]["window"][1], detections[idx]["window"][0], detections[idx]["window"][3], detections[idx]["window"][2], predictions[idx,cls]) )  for idx in bbs_idx]
+        for detection in list(nms_detections(np.array(bbs_for_nms))):
+            score = detection[4]
+            bb = detection[:4].astype(int)
+            resulting_bbs.append((bb,score,synset_dict[cls]))
+    
+    print "Resulting detections:",resulting_bbs
+    
+    image = cv2.cvtColor(image, cv2.cv.CV_RGB2BGR)
+    
+    for bb, det_score, class_name in resulting_bbs:
+        clr = (randint(0,255), randint(0,255), randint(0,255))
+        cv2.putText(image,"%s"%class_name, (int(bb[0])+ 10,int(bb[1]) + 10), cv2.FONT_HERSHEY_TRIPLEX, 0.6, clr)
+        cv2.rectangle(image,(int(bb[0]),int(bb[1])),(int(bb[2]),int(bb[3])),color=clr)
+
+    cv2.imwrite(args.output_image_file, image)
+
+    cv2.imshow("Detection result",image)
+    cv2.waitKey()
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv)
---
+++ source/detector.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python
+"""
+This a version of the caffe python detector modified by Alessandro Ferrari 
+(alessandroferrari87@gmail.com).
+
+Example usage:
+
+python detect.py --crop_mode=bing --pretrained_model=/home/alessandro/caffe/models/bvlc_reference_rcnn_ilsvrc13/bvlc_reference_rcnn_ilsvrc13.caffemodel --model_def=/home/alessandro/caffe/models/bvlc_reference_rcnn_ilsvrc13/deploy.prototxt --mean_file=/home/alessandro/caffe/python/caffe/imagenet/ilsvrc_2012_mean.npy --gpu --raw_scale=255 --weights_1st_stage_bing /opt/Datasets/VOC2007/BING_Results/weights.txt --sizes_idx_bing /opt/Datasets/VOC2007/BING_Results/sizes.txt --weights_2nd_stage_bing /opt/Datasets/VOC2007/BING_Results/2nd_stage_weights.json  /opt/Datasets/VOC2007/BING_Results/images.txt /opt/Datasets/VOC2007/BING_Results/det_output.h5
+
+Do windowed detection by classifying a number of images/crops at once,
+optionally using the selective search window proposal method.
+
+This implementation follows ideas in
+    Ross Girshick, Jeff Donahue, Trevor Darrell, Jitendra Malik.
+    Rich feature hierarchies for accurate object detection and semantic
+    segmentation.
+    http://arxiv.org/abs/1311.2524
+
+The selective_search_ijcv_with_python code required for the selective search
+proposal mode is available at
+    https://github.com/sergeyk/selective_search_ijcv_with_python
+"""
+
+import os
+import cv2
+import sys
+import time
+import caffe
+import warnings
+import numpy as np
+from skimage import img_as_float
+try:
+    from bing import Bing
+    bing_flag = True
+except Exception:
+    warnings.warn("Impossible to import bing.")
+    bing_flag = False
+
+def resize_image(image):
+    
+    h,w,nch = image.shape
+    max_edge = max(w,h)
+    ratio = 500.0 / max_edge
+    new_w = ratio * w
+    new_h = ratio * h
+    image = cv2.resize(image,(int(new_w),int(new_h)), interpolation=cv2.INTER_CUBIC)
+    
+    return image
+
+class Detector(caffe.Net):
+    """
+    Detector extends Net for windowed detection by a list of crops or
+    selective search proposals.
+    """
+    def __init__(self, model_file, pretrained_file, gpu=False, mean=None,
+                 input_scale=None, raw_scale=None, channel_swap=None,
+                 context_pad=None, weights_1st_stage_bing = None, sizes_idx_bing = None,
+                 weights_2nd_stage_bing = None, 
+                 num_bbs_psz_bing = 130, num_bbs_final_bing = 1500):
+        """
+        Take
+        gpu, mean, input_scale, raw_scale, channel_swap: params for
+            preprocessing options.
+        context_pad: amount of surrounding context to take s.t. a `context_pad`
+            sized border of pixels in the network input image is context, as in
+            R-CNN feature extraction.
+        """
+        caffe.Net.__init__(self, model_file, pretrained_file)
+        self.set_phase_test()
+
+        if gpu:
+            self.set_mode_gpu()
+        else:
+            self.set_mode_cpu()
+
+        if mean is not None:
+            self.set_mean(self.inputs[0], mean)
+        if input_scale is not None:
+            self.set_input_scale(self.inputs[0], input_scale)
+        if raw_scale is not None:
+            self.set_raw_scale(self.inputs[0], raw_scale)
+        if channel_swap is not None:
+            self.set_channel_swap(self.inputs[0], channel_swap)
+
+        self.configure_crop(context_pad)
+        
+        if bing_flag and not weights_1st_stage_bing is None and not sizes_idx_bing is None and not weights_2nd_stage_bing is None:
+            self.bing = Bing(weights_1st_stage = weights_1st_stage_bing, sizes_idx = sizes_idx_bing ,weights_2nd_stage = weights_2nd_stage_bing, num_bbs_per_size_1st_stage= num_bbs_psz_bing, num_bbs_final = num_bbs_final_bing)
+        else:
+            self.bing = None
+        
+    def detect_bing(self, image):
+
+        assert not self.bing is None
+        
+        if not bing_flag:
+            print "Bing detection invoked but error while importing bing module!"
+            sys.exit(1)
+                
+        
+        t0 = time.time()
+        bbs = self.bing.predict(image)
+        t1 = time.time()
+        print "Bing prediction: {0:.2f}s.".format(t1-t0)
+        images_windows = self.detect_windows(image, bbs)
+        
+        return self.get_predictions_from_cropped_images(images_windows)
+        
+    def detect_windows(self, image, bbs):
+        """
+        Do windowed detection over given images and windows. Windows are
+        extracted then warped to the input dimensions of the net.
+
+        Take
+        images_windows: (image filename, window list) iterable.
+        context_crop: size of context border to crop in pixels.
+
+        Give
+        detections: list of {filename: image filename, window: crop coordinates,
+            predictions: prediction vector} dicts.
+        """
+        images_windows = []
+        
+        image_fl = img_as_float(image)
+        t0 = time.time()
+        for bb in bbs:
+            bb = np.array((bb[1],bb[0],bb[3],bb[2]))
+            images_windows.append((self.crop(image_fl, bb), bb))
+        t1 = time.time()
+        print "Bounding boxes cropping: {0:.2f}s.".format(t1-t0)
+
+        return images_windows
+    
+    def get_predictions_from_cropped_images(self, images_windows):
+        
+        # Run through the net (warping windows to input dimensions).
+        caffe_in = np.zeros((len(images_windows), images_windows[0][0].shape[2])
+                            + self.blobs[self.inputs[0]].data.shape[2:],
+                            dtype=np.float32)
+        bbs = []
+        for ix, (window_in, bb) in enumerate(images_windows):
+            caffe_in[ix] = self.preprocess(self.inputs[0], window_in)
+            bbs.append(bb)
+        out = self.forward_all(**{self.inputs[0]: caffe_in})
+        predictions = out[self.outputs[0]].squeeze(axis=(2,3))
+
+        # Package predictions with images and windows.
+        detections = []
+        ix = 0
+        for bb in bbs:
+            detections.append({
+                'window': bb,
+                'prediction': predictions[ix],
+            })
+            ix += 1
+        return detections, predictions
+
+    def crop(self, im, window):
+        """
+        Crop a window from the image for detection. Include surrounding context
+        according to the `context_pad` configuration.
+
+        Take
+        im: H x W x K image ndarray to crop.
+        window: bounding box coordinates as ymin, xmin, ymax, xmax.
+
+        Give
+        crop: cropped window.
+        """
+        # Crop window from the image.
+        crop = im[window[0]:window[2], window[1]:window[3]]
+
+        if self.context_pad:
+            box = window.copy()
+            crop_size = self.blobs[self.inputs[0]].width  # assumes square
+            scale = crop_size / (1. * crop_size - self.context_pad * 2)
+            # Crop a box + surrounding context.
+            half_h = (box[2] - box[0] + 1) / 2.
+            half_w = (box[3] - box[1] + 1) / 2.
+            center = (box[0] + half_h, box[1] + half_w)
+            scaled_dims = scale * np.array((-half_h, -half_w, half_h, half_w))
+            box = np.round(np.tile(center, 2) + scaled_dims)
+            full_h = box[2] - box[0] + 1
+            full_w = box[3] - box[1] + 1
+            scale_h = crop_size / full_h
+            scale_w = crop_size / full_w
+            pad_y = round(max(0, -box[0]) * scale_h)  # amount out-of-bounds
+            pad_x = round(max(0, -box[1]) * scale_w)
+
+            # Clip box to image dimensions.
+            im_h, im_w = im.shape[:2]
+            box = np.clip(box, 0., [im_h, im_w, im_h, im_w])
+            clip_h = box[2] - box[0] + 1
+            clip_w = box[3] - box[1] + 1
+            assert(clip_h > 0 and clip_w > 0)
+            crop_h = round(clip_h * scale_h)
+            crop_w = round(clip_w * scale_w)
+            if pad_y + crop_h > crop_size:
+                crop_h = crop_size - pad_y
+            if pad_x + crop_w > crop_size:
+                crop_w = crop_size - pad_x
+
+            # collect with context padding and place in input
+            # with mean padding
+            context_crop = im[box[0]:box[2], box[1]:box[3]]
+            context_crop = caffe.io.resize_image(context_crop, (crop_h, crop_w))
+            crop = self.crop_mean.copy()
+            crop[pad_y:(pad_y + crop_h), pad_x:(pad_x + crop_w)] = context_crop
+
+        return crop
+
+    def configure_crop(self, context_pad):
+        """
+        Configure amount of context for cropping.
+        If context is included, make the special input mean for context padding.
+
+        Take
+        context_pad: amount of context for cropping.
+        """
+        self.context_pad = context_pad
+        if self.context_pad:
+            raw_scale = self.raw_scale.get(self.inputs[0])
+            channel_order = self.channel_swap.get(self.inputs[0])
+            # Padding context crops needs the mean in unprocessed input space.
+            mean = self.mean.get(self.inputs[0])
+            if mean is not None:
+                crop_mean = mean.copy().transpose((1,2,0))
+                if channel_order is not None:
+                    channel_order_inverse = [channel_order.index(i)
+                                            for i in range(crop_mean.shape[2])]
+                    crop_mean = crop_mean[:,:, channel_order_inverse]
+                if raw_scale is not None:
+                    crop_mean /= raw_scale
+                self.crop_mean = crop_mean
+            else:
+                self.crop_mean = np.zeros(self.blobs[self.inputs[0]].data.shape,
+                                          dtype=np.float32)
